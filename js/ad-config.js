12607d4f70ee30dec29f1b258e5bad9f87be40d7
Eva Daniel <61015593+etkhdaniel@users.noreply.github.com>
2026-07-24 18:28:00 -0400

    Configure AdSense publisher ID

--- js/ad-config.js
+++ js/ad-config.js
@@ -2,6 +2,6 @@
 // Publisher IDs look like: ca-pub-1234567890123456
 // Ad slot IDs are numeric.
 export const AD_CONFIG = {
-  client: 'ca-pub-REPLACE_WITH_YOUR_PUBLISHER_ID',
+  client: 'ca-pub-7849492850838493',
   slot: 'REPLACE_WITH_YOUR_AD_SLOT_ID',
 };
